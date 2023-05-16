3f968f2f1be9f492601df431788d7e56bc9a3f6a
ChaliMsani <chalisani@hotmail.com>
2023-05-16 23:13:40 +0200

    added footer to the app page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <Contact />
       <About />
+      <Footer />
 </BrowserRouter>
   );
 }
