633a2d6d25158d94073d3ee36570d1caaca99b63
ChaliMsani <chalisani@hotmail.com>
2023-05-17 21:39:44 +0200

    added a textdecoration none to the button link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import header from './images/header.jpeg';
 const Header = () => {
     return (
      <div className="container">
-        <h1>
-            Chali Mohamedsani
-        </h1>
+       
         <img className='headerimg' src={header} alt="first image" />
      </div>
     );
