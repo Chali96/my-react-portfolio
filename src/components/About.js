7be2916b024485a1a989b2e8931790962fd65d0e
ChaliMsani <chalisani@hotmail.com>
2023-05-17 22:12:09 +0200

    done with about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,33 @@
 import Header from './Header';
+import image3 from './images/image3.jpeg'
+import image4 from './images/image4.jpeg'
 
 const About = () => {
     return (
     <div className='container'>
-        <h4>
-            About page!
-        </h4>
+        <h3>
+            Get to know me
+        </h3>
+        <p>I will write 1 truth and 1 lie about me. <br/>
+            Hover over the button you believe is the truth about me.
+            <br/>
+            1. I am 1.80 centimeter
+            <br/>
+            2. I dont like lasagne
+         </p>
+        <div className='aboutbtn'>
+        <button className='btn1'> </button>
+        <button className='btn2'> </button>
+        </div>
+        <p>Green is true & red is false</p>
+       
+       <div className='aboutimg'>
+       <img className='aboutimg' src={image3} alt="first image" />
+        <img className='aboutimg' src={image4} alt="first image" />
+       </div>
+      
+
+
     </div>
     );
 };
