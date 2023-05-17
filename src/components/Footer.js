99131b1450e3005feede2d88d5b0e4659fdbc74e
ChaliMsani <chalisani@hotmail.com>
2023-05-17 21:33:21 +0200

    the icons has now links to each page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,28 @@
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+
 
 
 const Footer = () => {
+  const handleFBClick = () => {
+    window.open('https://www.facebook.com', '_blank');
+  };
+
+
+  const handleIGClick = () => {
+    window.open('http://www.instagram.com', '_blank')
+  }
+
+  const handleTWClick = () => {
+    window.open('http://www.twitter.com', '_blank')
+  }
+
     return (
         <div className="footer">
-                 <FaFacebook />
-                <FaInstagram />
-                <FaTwitter />
+            <FaFacebook onClick={handleFBClick} style={{ cursor: 'pointer' }} />
+      <FaInstagram onClick={handleIGClick} style={{ cursor: 'pointer' }} />
+      <FaTwitter onClick={handleIGClick} style={{ cursor: 'pointer' }} />
         </div>
     );
 };
