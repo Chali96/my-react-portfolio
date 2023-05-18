83f256ede793b1fefa06d9e17d7929a59abe842b
ChaliMsani <chalisani@hotmail.com>
2023-05-18 23:21:39 +0200

    added a form to contsct page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,56 @@
+import { useState } from "react";
 
 
 const Contact = () => {
+  const [name, setName] = useState ('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('')
+  const [submittedMessage, setSubmittedMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefaulr();
+
+
+    setSubmittedMessage(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`)
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
     return (
         <div className='container'>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input
+                     type={name}
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
+               <br/>
+               <label>
+                    Email:
+                    <input
+                     type={email}
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+           <br/>
+           <label>
+                    Message:
+                    <textarea
+                     value={message}
+                     onChange={(e) => setMessage(e.target.value)}
+                    />
+                </label>
+
+              <br/>
+              <button type="submit">Submit</button>  
+              </form>
+
             <h3>
                 
             </h3>
