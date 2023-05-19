fd135ef63d56105de5d266c7afd7c132771f9d20
ChaliMsani <chalisani@hotmail.com>
2023-05-19 13:02:19 +0200

    completly done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import header from './images/header.jpeg';
 import { Link } from 'react-router-dom';
 
-
-
-
 const Header = () => {
     return (
      <div className="container">
+          {/* the nav bar */}
           <div className='navbar'>
+
+            {/*the three links */}
           <Link className='btntext' to="/">
             
             <button className='navbtn'>Home</button>
@@ -20,8 +20,6 @@ const Header = () => {
             
             <button className='navbtn'>Contact me</button>
            </Link>  
-          
-           
             </div>
         <img className='headerimg' src={header} alt="first image" />
       
