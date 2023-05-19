5dfdc53a8a20bbb0897043089eb7c50d453f73c9
ChaliMsani <chalisani@hotmail.com>
2023-05-19 12:48:26 +0200

    comments added to about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,14 @@
-import Header from './Header';
 import image3 from './images/image3.jpeg'
 import image4 from './images/image4.jpeg'
 
 const About = () => {
     return (
     <div className='container'>
+        {/* the heading */}
         <h3>
             Get to know me
         </h3>
+
         <p>I will write 1 truth and 1 lie about me. <br/>
             Hover over the button you believe is the truth about me.
             <br/>
@@ -15,13 +16,18 @@ const About = () => {
             <br/>
             2. I dont like lasagne
          </p>
-         
+
+                 {/*the buttons where the user guesses correct answer */}
         <div className='aboutbtn'>
         <button className='btn1'> 1 </button>
         <button className='btn2'> 2 </button>
         </div>
+
+
+                {/* The user gets explaintion about the buttons here */}
         <p>Green is true & red is false</p>
        
+               {/*the two images */}
        <div className='aboutimg'>
        <img className='aboutimg' src={image3} alt="first image" />
         <img className='aboutimg' src={image4} alt="first image" />
