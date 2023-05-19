9419368bd5ea067f2f4034c2ac2323cb38b91460
ChaliMsani <chalisani@hotmail.com>
2023-05-19 12:53:50 +0200

    app done

--- src/App.js
+++ src/App.js
@@ -13,25 +13,35 @@ function App() {
   
   return (
 <BrowserRouter>
+{/* the modal component */}
 <div className='container-modal'>
   {status && (
+    // the modal is rendered when status is true
     <Modal close={() => setStatus(false)}>
       <div className='container-modal'>
         <p>Hey, I hope you have a beautiful day today!</p>
       </div>
     </Modal>
   )}
+  {/*button is to trigger the modall */}
 <button onClick={() => setStatus(true)} className="btn">For you!</button>
 </div>
 <Header />
 <Routes>
+  
+  {/*routes to the home, contact, and about page */}
   <Route path='/' element={<Home/>} />
   <Route path='/contact' element={<Contact/>} />
   <Route path='/about' element={<About/>} />
 </Routes>
+
+{/*footer component */}
 <Footer />
 </BrowserRouter>
   );
 }
 
 export default App;
+
+        {/* */}
+
